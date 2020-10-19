1a57649eb480b3a535e4c5303a9c5e0af8ee6b92
Shuqi-HUANG <1155155806@link.cuhk.edu.hk>
2020-10-19 18:53:34 +0800

    Create photography.js

---
+++ COMM5961/Assignment05/photography.js
@@ -0,0 +1,5 @@
+function table(){
+    var selecter = parent.document.getElementById("selecter");
+                                if (selecter.value=="0"){document.getElementById("iframe").src="https://airtable.com/embed/shrdp0KG7WWPYnDFD?backgroundColor=green&viewControls=on"};
+                                if (selecter.value=="1"){document.getElementById("iframe").src="https://airtable.com/embed/shrfcsKjwOPyJX53n?backgroundColor=yellow&viewControls=on"};
+                                if (selecter.value=="2"){document.getElementById("iframe").src="https://airtable.com/embed/shr79AEbINyMCnern?backgroundColor=blue&viewControls=on"};}
